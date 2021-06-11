a07207f724b82c7bf9693c5ea971ec47a7e4d08d
mnaibei <69506217+mnaibei@users.noreply.github.com>
2021-06-11 10:12:09 +0300

    Add files via upload

---
+++ behavior.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+	var thumbnailElement = document.getElementById("smart_thumbnail");
+	thumbnailElement.addEventListener("click", function() {
+		if (thumbnailElement.className == "") {
+			thumbnailElement.className = "small";
+		}else{
+			thumbnailElement.className = "";
+		}
+});
+
+});
+
+
